3cac7083feec59af8443afc8c85a840928c1fe1e
Christopher Grabski <grabc59@gmail.com>
2016-10-17 21:53:35 -0600

    styling changes, adding erase

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ var currentColor = document.querySelector('#currentColor');
 var palette = document.querySelector('#palette');
 var canvas = document.querySelector('#canvas');
 var savedColor = '';
+var savedBorder = '';
 var colors = ['black', 'white', 'grey', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 var userColor = '';
+var colorWheel = document.getElementById('color-wheel');
 
 
 
 function populateCanvas() {
-  for (var i = 0; i < 924; i++) {
+  for (var i = 0; i < 2263; i++) {
     var div = document.createElement('div');
     canvas.appendChild(div);
     div.className = 'pixel';
@@ -37,7 +39,7 @@ populateCanvas();
 function createColors() {
   for (var i = 0; i < colors.length; i++) {
     var div = document.createElement('div');
-    palette.appendChild(div);
+    palette.insertBefore(div, colorWheel);
     div.className = 'colors';
     div.style['background-color'] = colors[i];
   }
@@ -51,6 +53,7 @@ createColors();
 ///////////////////////////////////////
 document.querySelector('#palette').addEventListener('mousedown', function (event) {
   savedColor = event.target.style['background-color'];
+  savedBorder = event.target.style['background-color'];
   currentColor.style['background-color'] = savedColor;
 });
 
@@ -59,12 +62,12 @@ document.querySelector('#palette').addEventListener('mousedown', function (event
 ///////////////////////////////////////
 function click (event) {
   event.target.style['background-color'] = savedColor;
-  event.target.style.border = '1px solid ' + savedColor;
+  event.target.style.border = '1px solid ' + savedBorder;
   document.querySelector('#canvas').addEventListener('mousemove', drag);
 }
 function drag (event) {
   event.target.style['background-color'] = savedColor;
-  event.target.style.border = '1px solid ' + savedColor;
+  event.target.style.border = '1px solid ' + savedBorder;
 }
 function stopPainting () {
   document.querySelector('#canvas').removeEventListener('mousemove', drag);
@@ -79,7 +82,19 @@ document.querySelector('#canvas').addEventListener('mouseup', stopPainting);
 document.querySelector('#add-color').addEventListener('mousedown', function() {
   userColor = document.getElementById('color-wheel').value;
   var div = document.createElement('div');
-  palette.appendChild(div);
+  palette.insertBefore(div, colorWheel);
   div.className = 'colors';
   div.style['background-color'] = userColor;
 });
+
+
+///////////////////////////////////////
+///////////// Erase
+///////////////////////////////////////
+
+//listen for click on the erase button
+document.querySelector('#erase').addEventListener('mousedown', function (event) {
+  savedColor = 'white';
+
+});
+//set the current color to white, and the border to black
